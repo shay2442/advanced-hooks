267051b35f6e715696fc7952013811961636097f
Shay <shay2442@yahoo.com>
2022-08-05 16:44:41 -0500

    Working weather and pokemon displayed

--- src/App.js
+++ src/App.js
@@ -1,23 +1,54 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState, useEffect } from "react";
 
 function App() {
+  const [count, setCount] = useState(0);
+  const [books, setBooks] = useState("");
+  const [pokemon, setPokemon] = useState("");
+  const [currentPokemonId, setCurrentPokemonId] = useState(1);
+  const city = "Austin";
+  const [weather, setWeather] = useState([]);
+  // const JokeAPI = require('sv443-joke-api');
+
+  useEffect(
+    (name) => {
+      fetch(`https://pokeapi.co/api/v2/pokemon/${currentPokemonId}`, {})
+        .then((r) => r.json())
+        .then((pokemon) => setPokemon(pokemon));
+    },
+    [currentPokemonId]
+  );
+
+  useEffect(() => {
+    fetch(`http://wttr.in/${city}?format=j1`, {})
+      .then((r) => r.json())
+      .then((weather) => setWeather(weather.current_condition[0]));
+    // console.log(weather.current_condition)
+  }, []);
+
+  // useEffect((name) => {
+  //   fetch("https://pokeapi.co/api/v2/pokemon", {})
+  //     .then((r) => r.json())
+  //     .then((pokemons) => {
+  //       setPokemons(pokemons.results);
+  //     });
+  // }, []);
+
+  function handleClick() {
+    setCurrentPokemonId(currentPokemonId + 1);
+  }
+  console.log(weather)
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <h3>Current Weather: {weather.FeelsLikeF}</h3>
+      <button onClick={handleClick}>Next Pokemon</button>
+      {/* {pokemons.map((pokemon) => {
+        return <h1>Pokemon: {pokemon.name}, {pokemon.url}</h1>
+      
+      })} */}
+      <h1>{pokemon.name}</h1>
+      {pokemon ? <img src={pokemon.sprites.front_default} /> : "No pokemon yet"}
+     
     </div>
   );
 }
